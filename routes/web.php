d0ddd80ec946ccde2160933f8922e20100742aea
Riankrn <150497135+Riankrn@users.noreply.github.com>
2023-11-24 15:09:29 +0700

    subbmission2

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/about', function () {
+    $data = [
+        'pageTitle' => 'oh jadi',
+        'content' => 'ini halaman tentang kami.'
+    ];
+    return view('about', $data);
+});
+Route::resource('users','UseController');
+Route::get('/produk', 'App\Http\Controllers\produkController@index');
+
+Route::get('/produk/tambah_produk', 'produkController@tambah');
+Route::post('/produk/simpan_produk', 'produkController@tambah');
+Route::get('/produk/ubah_produk', 'produkController@ubah');
+Route::post('/produk/update_produk', 'produkController@update');
